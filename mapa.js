e9c978c96b4ce2ff4ef2d5a036c25da4efb7fbab
Fonta22 <paufontasp@gmail.com>
2022-04-16 11:54:18 +0200

    Add JavaScript

---
+++ mapa.js
@@ -0,0 +1,18 @@
+// Beograd: 44.787197, 20.457273
+// Srbija: 44.2108, 20.9224
+const coords = {
+    lat: 44.2108,
+    lon: 20.9224
+}
+
+const mymap = L.map('SrbijaMapa').setView([coords.lat, coords.lon], 6);
+
+const attribution =
+    '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+
+tiles.addTo(mymap);
+
+const marker = L.marker([coords.lat, coords.lon]).addTo(mymap);
